55fbc452ce9ad27c604cac4218c963bae831729e
Mohamed DeFranceschi <mohamed.de-franceschi@etu.unistra.fr>
2024-02-08 19:17:49 +0100

    Exercice 1 fini dans le main.c

--- TP1/main.c
+++ TP1/main.c
@@ -3,10 +3,37 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
 
-int main(){
+#define CHK(op) do { if ((op) == -1) raler (#op); } while (0)
 
+
+void raler (const char * msg)
+{
+    perror (msg);
+    exit (EXIT_FAILURE);
+}
+
+
+int main(int argc, char* argv[]){
+
+    int buffer_size;
+    char buff[buffer_size];
+
+    int fichierSource;
+    int fichierDestination;
+    fichierSource = open(argv[1], O_RDONLY);
     
+
+    fichierDestination = open("fichierCreer.txt", O_WRONLY | O_TRUNC | O_CREAT, 0666);
+
+    int nb_octet = read(fichierSource, buff, buffer_size);
+
+    fichierDestination = write(fichierDestination, buff, nb_octet);
+
+    CHK(close(fichierSource));
+    CHK(close(fichierDestination));
+
 
     return 0;
 
